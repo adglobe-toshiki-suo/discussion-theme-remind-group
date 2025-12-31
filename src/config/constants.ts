97e9bea97c669c73e4c81bd1e7da339ac4239b73
toshiki.suo <toshiki.suo@adglobe.co.jp>
2025-12-31 09:57:13 +0000

    fix: 定数にas constの定数宣言を付与およびスプレッドシートのシート名をsnake caseに統一

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,16 +1,14 @@
-// 1週間後の日付
-export const NEXT_WEEK = 7;
 // SL1座談会スケジュールシート
-export const SL1_SCHEDULE_SHEET_NAME = "Section1_schedule";
+export const SL1_SCHEDULE_SHEET_NAME = "section1_schedule" as const;
 // SL1メンバーマスターシート
-export const SL1_MEMBER_SHEET_NAME = "Section1_member_master";
+export const SL1_MEMBER_SHEET_NAME = "section1_member_master" as const;
 // SL2座談会スケジュールシート
-export const SL2_SCHEDULE_SHEET_NAME = "Section2_schedule";
+export const SL2_SCHEDULE_SHEET_NAME = "section2_schedule" as const;
 // SL2メンバーマスターシート
-export const SL2_MEMBER_SHEET_NAME = "Section2_member_master";
+export const SL2_MEMBER_SHEET_NAME = "section2_member_master" as const;
 // スプレッドシート表ヘッダーINDEX
-export const HEADER_ROW_INDEX = 0;
+export const HEADER_ROW_INDEX = 0 as const;
 // スプレッドシート表カラムINDEX
-export const FIRST_DATA_ROW_INDEX = 1;
+export const FIRST_DATA_ROW_INDEX = 1 as const;
 // スケジュールシート日付ヘッダー
-export const DATE_COLUMN_KEY = "date";
+export const DATE_COLUMN_KEY = "date" as const;
